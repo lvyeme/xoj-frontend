c988fc3527e1907f228a644b99aa57e3f5300516
Sagasu <62923489+lvyeme@users.noreply.github.com>
2024-03-05 08:00:11 -0600

    The back-end connection is complete

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,19 +1,29 @@
 // initial state
 import { StoreOptions } from "vuex";
 import accessEnum from "@/access/accessEnum";
+import { UserControllerService } from "../../generated";
+import * as assert from "assert";
+import ACCESS_ENUM from "@/access/accessEnum";
 
 export default {
   namespaced: true,
   state: () => ({
     loginUser: {
       userName: "未登录",
-      userRole: accessEnum.NOT_LOGIN,
     },
   }),
   actions: {
-    getLoginUser({ commit, state }, payload) {
-      //todo 改为从远程获取登录信息
-      commit("updateUser", payload);
+    async getLoginUser({ commit, state }, payload) {
+      // 从远程登录获取用户信息
+      const res = await UserControllerService.getLoginUserUsingGet();
+      if (res.code === 0) {
+        commit("updateUser", res.data);
+      } else {
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: ACCESS_ENUM.NOT_LOGIN,
+        });
+      }
     },
   },
   mutations: {
